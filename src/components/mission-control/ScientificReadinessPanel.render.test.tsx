76d9bec93469cd5e69547f34a0b606cf72261087
Jeff Parham <jsp1440@aol.com>
2026-09-12 02:09:14 -0700

    test: run readiness render checks in jsdom

--- src/components/mission-control/ScientificReadinessPanel.render.test.tsx
+++ src/components/mission-control/ScientificReadinessPanel.render.test.tsx
@@ -1,3 +1,5 @@
+// @vitest-environment jsdom
+
 import React, { act } from 'react';
 import { createRoot, type Root } from 'react-dom/client';
 import { afterEach, beforeEach, describe, expect, it } from 'vitest';
